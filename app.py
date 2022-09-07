e1061ac67bbdf520da11b446d41024f64e273c6b
SatyaNerurkar <satyanerurkar@gmail.com>
2022-09-07 12:05:13 +0530

    First Commit

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS,cross_origin
 import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
+import re
 
 app = Flask(__name__)
 
@@ -41,22 +42,18 @@ def index():
                 try:
                     #name.encode(encoding='utf-8')
                     name = commentbox.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text
-
                 except:
                     name = 'No Name'
 
                 try:
                     #rating.encode(encoding='utf-8')
                     rating = commentbox.div.div.div.div.text
-
-
                 except:
                     rating = 'No Rating'
 
                 try:
                     #commentHead.encode(encoding='utf-8')
                     commentHead = commentbox.div.div.div.p.text
-
                 except:
                     commentHead = 'No Comment Heading'
                 try:
@@ -69,6 +66,12 @@ def index():
                 mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
                           "Comment": custComment}
                 reviews.append(mydict)
+
+                custComment = deEmojify(custComment)
+
+                record = searchString +","+ name +","+ rating +","+ commentHead +","+ custComment + "\n"
+                fw.write(record)
+
             return render_template('results.html', reviews=reviews[0:(len(reviews)-1)])
         except Exception as e:
             print('The Exception message is: ',e)
@@ -77,6 +80,30 @@ def index():
 
     else:
         return render_template('index.html')
+
+# Function to remove any emoticon or symbols present in comment.
+def deEmojify(text):
+    regrex_pattern = re.compile(pattern = "["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        u"\U00002500-\U00002BEF"  # chinese char
+        u"\U00002702-\U000027B0"
+        u"\U00002702-\U000027B0"
+        u"\U000024C2-\U0001F251"
+        u"\U0001f926-\U0001f937"
+        u"\U00010000-\U0010ffff"
+        u"\u2640-\u2642" 
+        u"\u2600-\u2B55"
+        u"\u200d"
+        u"\u23cf"
+        u"\u23e9"
+        u"\u231a"
+        u"\ufe0f"  # dingbats
+        u"\u3030"
+                      "]+", flags = re.UNICODE)
+    return regrex_pattern.sub(r'',text)
 
 if __name__ == "__main__":
     #app.run(host='127.0.0.1', port=8001, debug=True)
